96c5d6011b09208bc8674723ffdd0949e4c3c579
Judit Vives <jvivesramon@gmail.com>
2024-06-10 22:10:15 +0200

    Add appRouter router and set App component as main route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.tsx";
 import "./styles/styles.scss";
+import { RouterProvider } from "react-router-dom";
+import appRouter from "./routers/appRouter.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>,
 );
