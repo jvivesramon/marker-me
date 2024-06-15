0e330f28abc71f72ff4b521de2445f2bba6fa9ff
Judit Vives <jvivesramon@gmail.com>
2024-06-15 19:41:53 +0200

    Add MarkerDetailPage to appRouter

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -2,7 +2,11 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import { Suspense } from "react";
 import path from "./paths/paths";
 import App from "../components/App/App";
-import { LazyMarkLisPage, LazyNotFoundPage } from "./lazyComponentes";
+import {
+  LazyMarkDetailPage,
+  LazyMarkLisPage,
+  LazyNotFoundPage,
+} from "./lazyComponentes";
 
 const appRouter = createBrowserRouter([
   {
@@ -18,6 +22,14 @@ const appRouter = createBrowserRouter([
         element: (
           <Suspense>
             <LazyMarkLisPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: `${path.markers}${path.detail}`,
+        element: (
+          <Suspense>
+            <LazyMarkDetailPage />
           </Suspense>
         ),
       },
