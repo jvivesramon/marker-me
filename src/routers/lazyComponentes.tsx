8998359a3cce56745ce28b329ecdef2a949ebce0
Judit Vives <jvivesramon@gmail.com>
2024-06-16 21:07:27 +0200

    Add ShoppingCartPage to route at appRouter

--- src/routers/lazyComponentes.tsx
+++ src/routers/lazyComponentes.tsx
@@ -8,6 +8,10 @@ export const LazyMarkDetailPage = lazy(
   () => import("../pages/MarkerDetailPage/MarkerDetailPage"),
 );
 
+export const LazyShoppingCartPage = lazy(
+  () => import("../pages/ShoppingCartPage/ShoppingCartPage"),
+);
+
 export const LazyNotFoundPage = lazy(
   () => import("../pages/NotFoundPage/NotFoundPage"),
 );
